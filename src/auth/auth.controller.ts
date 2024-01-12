d506b5488b0dacfbf53c21d98f7fb2160a15b8d2
Diegucho7 <87243460+Diegucho7@users.noreply.github.com>
2024-01-11 23:39:51 -0500

    enableCors

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards,Request } 
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginDto, UpdateAuthDto, RegisterUserDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
+import { LoginResponse } from './interfaces/login-response';
+import { User } from './entities/user.entity';
 
 
 @Controller('auth')
@@ -30,6 +32,17 @@ export class AuthController {
     // const user = req ['user'];
     // return user;  
     return this.authService.findAll();
+
+  }
+  @UseGuards( AuthGuard )
+  @Get('check-token')
+  checkToken(@Request()req: Request): LoginResponse {
+    
+    const user = req['user'] as User;
+    return{
+      user,
+      token: this.authService.getJwtToken({id: user._id})
+    }
 
   }
 
